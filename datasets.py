acf4b08acd3d779b82059d3c5815e517acded663
jeongwook lee <rightlit@gmail.com>
2021-02-05 14:41:52 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -117,12 +117,14 @@ class TextDataset(Dataset):
 
     def load_bbox(self):
         data_dir = self.data_dir
-        bbox_path = os.path.join(data_dir, 'CUB_200_2011', 'CUB_200_2011', 'bounding_boxes.txt')
+        #bbox_path = os.path.join(data_dir, 'CUB_200_2011', 'CUB_200_2011', 'bounding_boxes.txt')
+        bbox_path = os.path.join(data_dir, 'CUB_200_2011', '', 'bounding_boxes.txt')
         df_bounding_boxes = pd.read_csv(bbox_path,
                                         delim_whitespace=True,
                                         header=None).astype(int)
         #
-        filepath = os.path.join(data_dir, 'CUB_200_2011', 'CUB_200_2011', 'images.txt')
+        #filepath = os.path.join(data_dir, 'CUB_200_2011', 'CUB_200_2011', 'images.txt')
+        filepath = os.path.join(data_dir, 'CUB_200_2011', '', 'images.txt')
         df_filenames = \
             pd.read_csv(filepath, delim_whitespace=True, header=None)
         filenames = df_filenames[1].tolist()
@@ -320,7 +322,8 @@ class TextDataset(Dataset):
         #
         if self.bbox is not None:
             bbox = self.bbox[key]
-            data_dir = '%s/CUB_200_2011/CUB_200_2011' % self.data_dir
+            #data_dir = '%s/CUB_200_2011/CUB_200_2011' % self.data_dir
+            data_dir = '%s/CUB_200_2011/' % self.data_dir
         else:
             bbox = None
             data_dir = self.data_dir
